63856fef948bf5806fe67761031cb62582da051f
joancasanelles <joan.casanelles92@gmail.com>
2022-05-06 17:42:10 +0200

    Commit 06.05.2022

--- FigS_preds_responses_relationship.R
+++ FigS_preds_responses_relationship.R
@@ -1,29 +1,102 @@
+### Remove all R objects in the workspace
+rm(list = ls())
+setwd("~/Dropbox/City4bees/Analyses/bees_switzerland/")
 
+### Packages
+require(raster)
+require(viridis)
+require(ggplot2)
+library(sp)
+library(sf)
+require("magrittr")
+require("ggpubr")
+library(egg)
+###Load data
 extend.raster=raster("DATA/Selected descriptors/Results_2022_04_28/belowgound.tif")
-
-
 div <- stack(x = "DATA/Selected descriptors/Results_2022_04_28/Diversity_stack_revised_Selected_descriptors.tif")
+climate = stack("~/Dropbox/City4bees/Analyses/bees_switzerland/DATA/Data ready for analyses/Climate_PCA_CH_stack.tif")
+vegetation=stack("DATA/Data ready for analyses/Plant_PC_19_revised.tif")
+beehive=stack("DATA/Data ready for analyses/beehive-2012-2018.tif")
+urban=stack("DATA/Data ready for analyses/Urban_perc_18.tif")
+### Rename responses
 names(div) = c("belowgound","cleptoparasite","FDis", "feeding_specialization", 
                "FEve", "FRic", "InvSimpson", "ITD","LCBD_fun" ,"LCBD_taxo","phenoduration",
                "phenostart", "Rao", "Richness", "Shannon",  "Simpson",
                "solitary", "TED", "tong_length", "TOP")
+### Create a data frame
 div.metrics=as.data.frame(div)
 div.metrics=na.omit(div.metrics)
-climate = stack("~/Dropbox/City4bees/Analyses/bees_switzerland/DATA/Data ready for analyses/Climate_PCA_CH_stack.tif")
+### Mask climate raster to CH extent
 masked.climate=mask(x =climate,extend.raster )
-climate.df=as.data.frame(masked.climate, coordinates(masked.climate))
+### Create a data frame
+climate.df=as.data.frame(masked.climate)
+climate.df=cbind(climate.df, coordinates(masked.climate))
+vegetation.df=as.data.frame(vegetation)
 climate.df=na.omit(climate.df)
+climate.df$coordsmerged=paste(climate.df$x, climate.df$y, sep="")
+vegetation.df=na.omit(vegetation.df)
 
-dat=cbind(div.metrics,climate.df)
+urban.df=as.data.frame(urban)
+urban.df=cbind(urban.df, coordinates(masked.climate))
+urban.df$coordsmerged=paste(urban.df$x, urban.df$y, sep="")
+urban.df=urban.df[urban.df$coordsmerged %in% climate.df$coordsmerged ,]
 
+beehive.df=as.data.frame(beehive)
+beehive.df=cbind(beehive.df, coordinates(masked.climate))
+beehive.df$coordsmerged=paste(beehive.df$x, beehive.df$y, sep="")
+beehive.df=beehive.df[beehive.df$coordsmerged %in% climate.df$coordsmerged ,]
+### Merge
+dat=cbind(div.metrics,climate.df,vegetation.df,beehive.df[,1:8], urban.df[,4])
+dat$ID= seq(from=1, to=nrow(dat))
+### Make a sample
+sample.id= sample(dat$ID, size = 10000)
+dat.random=dat[dat$ID %in% sample.id,]
+### Create a palette
 
-palette_predictors_climate=c( "#1E1F26","#283655","#4D648D","#D0E1F9")
+for(r in 1:length(names(div))){
+p.clim=  ggplot(data = dat.random, aes(y=get(paste(names(div)[r])), x = Climate_PCA_CH_stack.1)) + 
+    geom_smooth(method="loess", span = 1, col="#1E1F26", fill="#1E1F26") +
+    geom_smooth(method="loess", span = 1, aes(x=Climate_PCA_CH_stack.2), col="#283655", fill="#283655") +
+    geom_smooth(method="loess", span = 1, aes(x=Climate_PCA_CH_stack.3), col="#4D648D", fill="#4D648D") +
+    geom_smooth(method="loess", span = 1, aes(x=Climate_PCA_CH_stack.4), col="#D0E1F9", fill="#D0E1F9") +
+    xlab("Environmental gradient") +
+    theme_classic(base_size = 20) +
+    theme(legend.title = element_blank()) +
+  ylab(paste(names(div)[r]))
 
-ggplot(data = dat, aes(y=TOP, x = Climate_PCA_CH_stack.4)) + 
-  geom_smooth(method="loess", span = 1, aes()) +
-  scale_color_manual(values =palette_predictors_climate) +
-  scale_fill_manual(values = palette_predictors_climate) +
-  scale_y_continuous(name = ylabel, breaks =labels , labels=labels, limits = c(min(labels), max(labels))) + 
+p.veg = ggplot(data = dat.random, aes(y=get(paste(names(div)[r])), x = Plant_PC_19_revised.2)) + 
+  geom_smooth(method="loess", span = 1, col="#1E1F26", fill="#1E1F26") +
+  geom_smooth(method="loess", span = 1, aes(x=Plant_PC_19_revised.3), col="#4B7447", fill="#4B7447") +
+  geom_smooth(method="loess", span = 1, aes(x=Plant_PC_19_revised.4), col="#7CAA2D", fill="#7CAA2D") +
+  xlab("Environmental gradient") +
   theme_classic(base_size = 20) +
   theme(legend.title = element_blank()) +
-  xlab("")
+  ylab(paste(names(div)[r]))
+
+p.lu = ggplot(data = dat.random,aes(y=get(paste(names(div)[r])), x = get('urban.df[, 4]'))) +
+  geom_smooth(method="loess", span = 1, col="#CDCDC0", fill="#CDCDC0") +
+ # geom_smooth(method="loess", span = 1, aes(x=Plant_PC_19_revised.3), col="#4B7447", fill="#4B7447") +
+#  geom_smooth(method="loess", span = 1, aes(x=Plant_PC_19_revised.4), col="#7CAA2D", fill="#7CAA2D") +
+  xlab("Prop. in 2500 m") +
+  theme_classic(base_size = 20) +
+  theme(legend.title = element_blank()) +
+  ylab(paste(names(div)[r]))
+
+p.beekeeping =  ggplot(data = dat.random, aes(y=get(paste(names(div)[r])), x =beehive.2012.2018.8)) +  
+  geom_smooth(method="loess", span = 1, col="#F4CC70", fill="#F4CC70") +
+  xlab("N. of beehives in 2500 m") +
+  theme_classic(base_size = 20) +
+  theme(legend.title = element_blank()) +
+  ylab(paste(names(div)[r]))
+
+plot.arranged=ggarrange(p.clim,p.veg,p.lu,p.beekeeping,
+                        nrow = 1, ncol=4)
+
+plot.arranged %>% ggexport(filename = paste("OUTPUT/Responses_predictors_direct/FigS",names(div[r]),"_predictors.png", sep=""),
+                                width = 1400, height = 400)
+plot.arranged %>% ggexport(filename = paste("OUTPUT/Responses_predictors_direct/FigS",names(div[r]),"_predictors.pdf", sep=""),
+                                width = 14, height = 4)
+}
+
+
+
