00ec4540a039abcd759c35c5d3734cbd4dc085ab
joancasanelles <joan.casanelles92@gmail.com>
2022-05-16 14:41:47 +0200

    commit 16.05.2022

--- Fig5bis_violin_elevation.R
+++ Fig5bis_violin_elevation.R
@@ -11,7 +11,7 @@
 ### ===================================
 # Remove all R objects in the workspace
 rm(list = ls())
-setwd("~/Dropbox/City4bees/Analyses/bees_switzerland/DATA/Selected descriptors/")
+setwd("~/Dropbox/City4bees/Analyses/bees_switzerland/")
 # Packages
 library(dplyr)
 require(raster)
@@ -128,9 +128,11 @@ for(r in 1:7){
   
  ggsave(filename = paste("OUTPUT/Violin_elevation/low_violin_lu_",colnames(responses.df.elevation.0.1000.lu)[r],".tiff", sep=""), plot = plot.low,device = "tiff", width = 9, height = 9, units = "in")
 }
-
-
-
+n.0.1000=responses.df.elevation.0.1000.lu %>% dplyr::group_by(Landuse_100x100) %>% dplyr::summarise(n=n())
+sum(n.0.1000$n)
+277264*100/1760226
+905274*100/1760226
+577688*100/1760226
 
 ### Mid elevation -----------------------------------------------------------------------
 responses.df.elevation.1000.2000.lu=merge(responses.df.elevation.1000.2000,LU.df,by=c("x", "y"))
@@ -153,6 +155,12 @@ for(r in 1:7){
   
   ggsave(filename = paste("OUTPUT/Violin_elevation/mid_violin_lu_",colnames(responses.df.elevation.1000.2000.lu)[r],".tiff", sep=""), plot = plot.mid,device = "tiff", width = 9, height = 9, units = "in")
 }
+n.1000.2000=responses.df.elevation.1000.2000.lu %>% dplyr::group_by(Landuse_100x100) %>% dplyr::summarise(n=n())
+sum(n.1000.2000$n)
+29420*100/1044795
+440311*100/1044795
+575064*100/1044795
+
 ### High elevation -----------------------------------------------------------------------
 responses.df.elevation.2000.4000=merge(responses.df.elevation.2000.4000,LU.df,by=c("x", "y"))
 responses.df.elevation.2000.4000=responses.df.elevation.2000.4000[responses.df.elevation.2000.4000$Landuse_100x100 != 4, c(3:10,12)]
@@ -174,3 +182,56 @@ for(r in 1:7){
   
   ggsave(filename = paste("OUTPUT/Violin_elevation/high_violin_lu_",colnames(responses.df.elevation.2000.4000)[r],".tiff", sep=""), plot = plot.high,device = "tiff", width = 9, height = 9, units = "in")
 }
+n.2000.4000=responses.df.elevation.2000.4000 %>% dplyr::group_by(Landuse_100x100) %>% dplyr::summarise(n=n())
+sum(n.2000.4000$n)
+
+1222*100/246487
+219560*100/246487
+25705*100/246487
+
+
+### -----------------------------------------------
+palette.lu=c("#ED5752", "#B38867", "#CDCDC0")
+d.per=data.frame(elevation=rep(c("0000-1000", "1000-2000", "2000-3500"), each=3),
+                 LU=rep(c("Urban", "Agriculutlral", "Forest")), 
+                 cover=c(15.75, 51.43, 32.82,2.82, 42.14, 55.04, 0.49, 89.07, 10.43))
+p1=ggplot(d.per[d.per$elevation=="0000-1000",], 
+       aes( x=cover, y=elevation, fill=LU)) + 
+  geom_bar(stat = "identity",width = 1.2) + 
+  scale_fill_manual(values = palette.lu) +
+  theme_classic(base_size = 20) +
+  xlab("Cover (%)") +
+  ylab("") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black")) 
+
+
+p2=ggplot(d.per[d.per$elevation=="1000-2000",], 
+       aes( x=cover, y=elevation, fill=LU)) + 
+  geom_bar(stat = "identity",width = 1.2) + 
+  scale_fill_manual(values = palette.lu) +
+  theme_classic(base_size = 20) +
+  xlab("Cover (%)") +
+  ylab("Elevation (m)") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black")) 
+
+p3=ggplot(d.per[d.per$elevation=="2000-3500",], 
+       aes( x=cover, y=elevation, fill=LU)) + 
+  geom_bar(stat = "identity",width = 1.2) + 
+  scale_fill_manual(values = palette.lu) +
+  theme_classic(base_size = 20) +
+  xlab("Cover (%)") +
+  ylab("") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black")) 
+
+FI=ggarrange(p1,p2,p3, nrow = 3, ncol = 1)
+
+require("magrittr")
+require("ggpubr")
+FI %>% ggexport(filename = "OUTPUT/Violin_plots/percentage.landcover.elevation.png",
+                    width = 1200, height = 400)
+
+FI %>% ggexport(filename = "OUTPUT/Violin_plots/percentage.landcover.elevation.pdf",
+                    width = 12, height = 4)
+
+
+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
